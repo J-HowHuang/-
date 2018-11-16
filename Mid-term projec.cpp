f10c4c52f6afd39cad5a714f0c1704c8d99c6e47
J-HowHuang <44998515+J-HowHuang@users.noreply.github.com>
2018-11-16 15:47:05 +0800

    Update Mid-term projec.cpp
    完成了input變數的宣告以及input

--- Mid-term projec.cpp
+++ Mid-term projec.cpp
@@ -1,2 +1,29 @@
 //Mid-term Project
-//test123123
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n = 0; //n: size of the map
+	int m = 0; //m: number of threats
+	int w = 0; //w: cost of changing direction
+	int d = 0; //d: the limit of flying distance
+	cin >> n >> m >> w >> d;
+	
+	int* x = new int [m]; //x[i]: the x-coordinate of the 'i'th threat
+	int* y = new int [m]; //y[i]: the y-coordinate of the 'i'th threat
+	int* r = new int [m]; //r[i]: the radius of the 'i'th threat
+	int* p = new int [m]; //p[i]: the power of the 'i'th threat
+	for(int i = 0; i < m; i++)
+		cin >> x[i];
+	for(int i = 0; i < m; i++)
+		cin >> y[i];
+	for(int i = 0; i < m; i++)
+		cin >> r[i];
+	for(int i = 0; i < m; i++)
+		cin >> p[i];
+	
+	int startX = 0, startY = 0; //the start point (startX, startY)
+	int endX = 0, endY = 0; //the end point (endX, endY)
+	cin >> startX >> startY >> endX >> endY;
+	
+}
