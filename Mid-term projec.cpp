2199bde736d9ce57c5b8c00ef0bdf114151f56b0
IMbeauty <45054771+IMbeauty@users.noreply.github.com>
2018-11-24 01:09:14 +0800

    add leftLen

--- Mid-term projec.cpp
+++ Mid-term projec.cpp
@@ -103,7 +103,9 @@ double threatof(int** route, int* x, int* y, int* r, int* p, int m, int w, int t
 			corner += corTemp;
 		}
 		double len = length(route[i][0], route[i][1], route[i + 1][0], route[i + 1][1]);
-		double cmpntX = (route[i + 1][0] - route[i][0]);
+		len += leftLen;
+		leftLen = 0;// leftLen歸零以便存下一段的leftLen 
+		double cmpntX = (route[i + 1][0] - route[i][0]);//culculate x component 
 		double cmpntY = (route[i + 1][1] - route[i][1]);
 		int intLen = static_cast<int>(len);
 		leftLen = len - intLen;
@@ -114,7 +116,6 @@ double threatof(int** route, int* x, int* y, int* r, int* p, int m, int w, int t
 			tempY += cmpntY / len;
 			threat += threatofP(tempX, tempY, x, y, r, p, m);
 		}
-		leftLen = 0;
 	}
 	threat += w * corner;
 	return threat;
