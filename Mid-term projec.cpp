950347a7a1f3b3bb60ccaf87f9a1cee058d7baf1
J-HowHuang <44998515+J-HowHuang@users.noreply.github.com>
2018-11-30 14:13:42 +0800

    second algorithm main function finished

--- Mid-term projec.cpp
+++ Mid-term projec.cpp
@@ -34,149 +34,127 @@ int main(){
 	int startX = 0, startY = 0; //the start point (startX, startY)
 	int endX = 0, endY = 0; //the end point (endX, endY)
 	cin >> startX >> startY >> endX >> endY;
-	cout << "input done\n";
-//a* algorithm	
-	//create openlist(1: open, 0: close, -1: not checked)
-	int** open = new int* [n + 1];
-	for(int i = 0; i < n + 1; i++){
-		open[i] = new int [n + 1];
-		for(int j = 0; j < n + 1; j++)
-			open[i][j] = -1;
-	}
-	
-	cout << "openlist done\n";
-//add start point to open list	
-	int openCnt = 1;
-	open[startX][startY] = 1;
-	
-	//f[][] is the min. approx cost of a point
-	double** f = new double* [n + 1];
-	for(int i = 0; i < n + 1; i++){
-		f[i] = new double [n + 1];
-		for(int j = 0; j < n + 1; j++)
-			f[i][j] = INFINITY;
-	}
-	int** originRoute = new int* [3];
-	for(int i = 0; i < 3; i++){
-		originRoute[i] = new int [2];
-	}
-	originRoute[0][0] = 0;
-	originRoute[0][1] = 0;
-	originRoute[1][0] = startX;
-	originRoute[1][1] = startY;
-	originRoute[2][0] = endX;
-	originRoute[2][1] = endY;
-
-	f[startX][startY] = threatof(originRoute, x, y, r, p, m, w, 0);
-	cout << threatof(originRoute, x, y, r, p, m, w, 0) << "\n";
-
-	
-	//source[][][0] is the x coordinate of the source of the point, while 1 is y
-	int*** source = new int** [n + 1];
-	for(int i = 0; i < n + 1; i++){
-		source[i] = new int*[n];
-		for(int j = 0; j < n + 1; j++){
-			source[i][j] = new int[2];
-			source[i][j][0] = startX;
-			source[i][j][1] = startY;
-		}
-	}
-	source[startX][startY][0] = -1;
-	source[startX][startY][1] = -1;
-	cout << "process 1 done\n";
-	while(true){
-		//search the point with min. approx cost in the open list
-		int currentX;
-		int currentY;
-		double minf = 99999999;
-		for(int i = 0; i < n; i++)//still need modified
-			for(int j = 0; j < n; j++)
-				if(open[i][j] == 1){
-					cout << "(" << i << ", " << j << ") f: " << f[i][j] << " source: (" << source[i][j][0] << ", " << source[i][j][1] << ")\n";
-					if(f[i][j] < minf){
-						minf = f[i][j];
-						currentX = i;
-						currentY = j;
-					}
-				}
-					
- 		cout << "current position: (" << currentX << ", " << currentY << ")\n";
-		//add it into close list
-		open[currentX][currentY] = 0;
-		openCnt -= 1;
-		//for each point near it
-		for(int i = -1; i <= 1; i++)
-			for(int j = -1; j <= 1; j++){
-				//if it is not in neither open list or close list
-				if(currentX + i < 0 || currentY + j < 0)
-					continue;
-				if(currentX + i > n || currentY + j > n)
-					continue;
-				if(open[currentX + i][currentY + j] != 0){
-					//add it to open list
-					open[currentX + i][currentY + j] = 1;
-					source[currentX + i][currentY + j][0] = currentX;
-					source[currentX + i][currentY + j][1] = currentY;
-					//calculate the f(t)
-					int nextsourceX = currentX + i;
-					int nextsourceY = currentY + j;
-					int turnCnt = 0;
-					int** tempRoute = new int* [MAX_CHANGING + 3];
-					for(int k = 0; k < MAX_CHANGING + 3; k++){
-						tempRoute[k] = new int[2];
-						tempRoute[k][0] = 0;
-						tempRoute[k][1] = 0;
-					}
-					//trace back from t and save the route in reverse	
-					tempRoute[1][0] = endX;
-					tempRoute[1][1] = endY;
-					while(nextsourceX != -1){
-						tempRoute[turnCnt + 2][0] = nextsourceX;
-						tempRoute[turnCnt + 2][1] = nextsourceY;
-						turnCnt += 1;
-						tempRoute[0][0] += 1;
-						tempRoute[0][1] += 1;
-						int tempX = source[nextsourceX][nextsourceY][0];
-						int tempY = source[nextsourceX][nextsourceY][1];
+//algorithm start
+    //remapping
+    int pmax = 0;
+    int *rp = new int [m];
+    for(int i = 0 ; i < m ; i++ )
+    {
+        if(p[i] > pmax)
+        {
+            pmax = p[i];
+            
+        }
+    }
+    for(int i = 0 ; i < m ; i++ )
+    {
+        rp[i] =r[i] * ( p[i] / pmax );
+    }
+    //open list
+    int** open = new int* [n + 1];
+    for(int i = 0; i < n + 1; i++){
+        open[i] = new int [n + 1];
+        for(int j = 0; j < n + 1; j++)
+            open[i][j] = -1;
+    }
+    //source list
+    int ***source = new int **[n + 1];
+    int ***f = new int **[n + 1];
+    for(int i = 0; i < n + 1; i++)
+    {
+        source[i] = new int *[n + 1];
+        f[i] = new int *[n + 1];
+        for(int j = 0; j < n + 1; j++){
+            source[i][j] = new int[2];
+            source[i][j][0] = startX;
+            source[i][j][1] = startY;
+            f[i][j] = new int [2];// 0:f,1:g
+            f[i][j][0] = INFINITY;
+            f[i][j][1] = 0;
+        }
+    }
+    source[startX][startY][0] = -1;
+    source[startX][startY][1] = -1;
+    
+    
+    //
+    
+    while( open[endX][endY] != 0 )
+    {
+        int currentX = 0;
+        int currentY = 0;
+        double minf = INFINITY;
+        for(int i = 0; i < n + 1; i++)//still need modified
+            for(int j = 0; j < n + 1; j++)
+                if(open[i][j] == 1)
+                {
+                    //    cout << "(" << i << ", " << j << ") f: " << f[i][j] << " source: (" << source[i][j][0] << ", " << source[i][j][1] << ")\n";
+                    if(f[i][j][0] < minf){
+                        minf = f[i][j][0];
+                        currentX = i;
+                        currentY = j;
+                    }
+                }
+        open[currentX][currentY] = 0;
+        for(int i = -unit; i <= unit; i += unit)
+            for(int j = -unit; j <= unit; j += unit)
+            {
+                //if it is not in neither open list or close list
+                //    cout << "(" << currentX + i << ", " <<currentY + j << ")\n";
+                if(currentX + i < 0 || currentY + j < 0)
+                    continue;
+                if(currentX + i > n || currentY + j > n)
+                    continue;
+                if(open[currentX + i][currentY + j] != 0)
+                {
+                    //add it to open list
+                    open[currentX + i][currentY + j] = 1;
+                    source[currentX + i][currentY + j][0] = currentX;
+                    source[currentX + i][currentY + j][1] = currentY;
+                    if(threatofP(currentX,currentY,x,y,p,m) != 0 )
+                    {
+                        open[currentX][currentY] = 0;
+                        continue;
+                    }
+                    int nextsourceX = currentX;
+                    int nextsourceY = currentY;
+                    int finalsourceX = 0;
+                    int finalsourceY = 0;
+                    while( nextsourceX != -1 )
+                    {
+                        // checking wall or not
+                        if(blocked)
+                        {
+                            int temp = nextsourceX;
+                            nextsourceX = source[nextsourceX][nextsourceY][0];
+                            nextsourceY = source[temp][nextsourceY][1];
+                        }
+                        else
+                        {
+                            finalsourceX = nextsourceX;
+                            finalsourceY = nextsourceY;
+                            int tempX = source[nextsourceX][nextsourceY][0];
+	                    	int tempY = source[nextsourceX][nextsourceY][1];
+							nextsourceX = tempX;
+							nextsourceY = tempY;
+                        }
+                    }
+                    source[currentX + i][currentY + j][0] = finalsourceX;
+                    source[currentX + i][currentY + j][1] = finalsourceY;
+                    nextsourceX = finalsourceX;
+                    nextsourceY = finalsourceY;
+                    while(nextsourceX != -1){
+                    	f[currentX + i][currentY + j][1] += length(nextsourceX, nextsourceY, source[nextsourceX][nextsourceY][0], source[nextsourceX][nextsourceY][1]);
+                    	int tempX = source[nextsourceX][nextsourceY][0];
+                    	int tempY = source[nextsourceX][nextsourceY][1];
 						nextsourceX = tempX;
 						nextsourceY = tempY;
 					}
-					turnCnt -= 1;
-					tempRoute[0][0] -= 1;
-					tempRoute[0][1] -= 1;
-					//reverse the temp route to get the route in correct order
-					int** route = new int* [MAX_CHANGING + 3];
-					for(int k = 0; k < MAX_CHANGING + 3; k++){
-						route[k] = new int [2];
-						route[k][0] = 0;
-						route[k][1] = 0;
-					}
-					route[0][0] = tempRoute[0][0];
-					route[0][1] = tempRoute[0][1];
-					for(int k = 0; k < turnCnt + 2; k++){
-						route[k + 1][0] = tempRoute[turnCnt + 2 - k][0];
-						route[k + 1][1] = tempRoute[turnCnt + 2 - k][1];
-					}
-					//if point t has better performance pass through former turn point straightly, update the source
-					for(int k = 0; k < turnCnt; k++){
-						double threatofRoute = threatof(route, x, y, r, p, m, w, k);
-					//	cout << "if ignore " << k << " corner " << threatofRoute << "\n";
-						if(threatofRoute < f[currentX + i][currentY + j]){
-							f[currentX + i][currentY + j] = threatofRoute;
-							source[currentX + i][currentY + j][0] = route[turnCnt - k][0];
-							source[currentX + i][currentY + j][1] = route[turnCnt - k][1];
-						}
-					}
-					
-				}
-				//if it is in open list 
-				
-			}
-		if(open[endX][endY] == 0)
-			break;
-	}
-	//trace back from t and save the route in reverse
-	int turnCnt = 0;	
+					f[currentX + i][currentY + j][0] = length(currentX + i, currentY + j, endX, endY) +  f[currentX + i][currentY + j][1];
+                }
+            }
+    }
+    int turnCnt = 0;	
 	int nextsourceX = source[endX][endY][0];
 	int nextsourceY = source[endX][endY][1];
 	int** tempRoute = new int* [MAX_CHANGING + 3];
@@ -217,13 +195,12 @@ int main(){
 	}
 	
 	cout << turnCnt << " ";
-	for(int i = 0; i < turnCnt + 1; i++){
+	for(int i = 1; i < turnCnt + 1; i++){
 		cout << route[i + 1][0] << " " << route[i + 1][1] << " ";
 	}
-	cout << route[turnCnt + 2][0] << " " << route[turnCnt + 2][1] << " ";
-	cout << threatof(route, x, y, r, p, m, w, 0);
-	return 0;
-	
+    
+
+    return 0;
 }
 double threatofP(double x0, double y0, int* x, int* y, int* r, int* p, int m){
 	double threat = 0;	
